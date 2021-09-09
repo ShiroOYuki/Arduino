0aed8f6c08806bf3cc5d42dfdc8bd610362b971e
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2021-09-09 14:54:14 +0800

    Update 4bit_7Segment.ino

--- 4bit_7Segment/4bit_7Segment.ino
+++ 4bit_7Segment/4bit_7Segment.ino
@@ -1,9 +1,29 @@
+int choose[5]={2,3,4,5};
+int var[5]={0,0,0,0};
+
 void setup() {
   // put your setup code here, to run once:
-
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  fourBit_7Segment(var,choose);
 }
+
+void fourBit_7Segment(int[] var,int choose[5]){
+  for(int i=0;i<4;i++){
+    pinMode(choose,OUTPUT);
+  }
+  for(int i=0;i<4;i++){
+    digitalWrite(choose[i],1);
+    //you can write some IC function here,
+    //value=var[i]
+    
+    //EX.
+    //someFunction(var[i])
+    
+    digitalWrite(choose[i],0);
+  }
+}
+
