fce3d3eb270c5608f722701f82cba801dfe4c031
ShiroOYuki <63907529+ShiroOYuki@users.noreply.github.com>
2021-10-15 13:01:31 +0800

    Update PIR.ino

--- PIR/PIR.ino
+++ PIR/PIR.ino
@@ -16,13 +16,13 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   delay(500);
-  int isAnyMove = digitalRead(PIR);
+  int isMoving = digitalRead(PIR);
   int v = analogRead(PIR);
-  Serial.print("isAnyMove:");
-  Serial.print(isAnyMove);
+  Serial.print("isMoving:");
+  Serial.print(isMoving);
   Serial.print("\tv:");
   Serial.println(v);
-  if(isAnyMove==0)
+  if(isMoving==0)
     digitalWrite(LED_R,1);
   else
     digitalWrite(LED_R,0);
